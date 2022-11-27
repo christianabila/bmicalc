861af19897b53a08b31247e8301504626bb9f98d
Christian Abila <christian@abila.at>
2022-11-27 16:17:11 +0100

    use Mustache in index.php

--- index.php
+++ index.php
@@ -48,149 +48,69 @@ try {
     header('Location: '.$_SERVER['PHP_SELF']);
     exit;
 }
+
+$mustacheEngine = new Mustache_Engine([
+    'entity_flags' => ENT_QUOTES,
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates'),
+]);
+
+if(isset($_SESSION['bmi'])) {
+    $bmi10 = $_SESSION['bmi'] * 10;
+
+    if ($bmi10 < 185) {
+        $underweight = 'green';
+    } elseif ($bmi10 >= 185 && $bmi10 <= 249) {
+        $normal = 'green';
+    } elseif ($bmi10 >= 250 && $bmi10 <= 299) {
+        $overweight = 'green';
+    } elseif ($bmi10 >= 300 && $bmi10 <= 349) {
+        $obese1 = 'green';
+    } elseif ($bmi10 >= 350 && $bmi10 <= 399) {
+        $obese2 = 'green';
+    } elseif ($bmi10 >= 400) {
+        $obese3 = 'green';
+    }
+} elseif(isset($_SESSION['error'])) {
+    echo $_SESSION['error'];
+    unset($_SESSION['error']);
+    
+    if(isset($_SESSION['height'])) {
+        unset($_SESSION['weight']);
+        unset($_SESSION['height']);   
+    } elseif(isset($_SESSION['heightFt'])) {
+        unset($_SESSION['weightEmp']);
+        unset($_SESSION['heightFt']);
+        unset($_SESSION['heightInch']);
+    }
+}
+
+$contextVariables = [
+    'version' => $VERSION,
+    'action' => $_SERVER['PHP_SELF'],
+    'bmi' => isset($_SESSION['bmi']) ? $_SESSION['bmi'] : '',
+    'dimension' => isset($_SESSION['dimension']) ? $_SESSION['dimension'] : '',
+    'selectedTab' => isset($_SESSION['selectedTab']) ? $_SESSION['selectedTab'] : 1,
+    'weight' => isset($_SESSION['weight']) ? $_SESSION['weight'] : '',
+    'height' => isset($_SESSION['height']) ? $_SESSION['height'] : '',
+    'weightEmpirical' => isset($_SESSION['weightEmp']) ? $_SESSION['weightEmp'] : '',
+    'heightFt' => isset($_SESSION['heightFt']) ? $_SESSION['heightFt'] : '',
+    'heightInch' => isset($_SESSION['heightInch']) ? $_SESSION['heightInch'] : '',
+    'underweight' => isset($underweight) ? $underweight : '',
+    'normal' => isset($normal) ? $normal : '',
+    'overweight' => isset($overweight) ? $overweight : '',
+    'obese1' => isset($obese1) ? $obese1 : '',
+    'obese2' => isset($obese2) ? $obese2 : '',
+    'obese3' => isset($obese3) ? $obese3 : '',
+];
+echo $mustacheEngine->render('main', $contextVariables);
+
+if(isset($_SESSION['bmi'])) {
+    unset($_SESSION['bmi']);
+    unset($_SESSION['weight']);
+    unset($_SESSION['height']);
+    unset($_SESSION['weightEmp']);
+    unset($_SESSION['heightFt']);
+    unset($_SESSION['heightInch']);
+    unset($_SESSION['dimension']);
+}
 ?>
-<!doctype html>
-<html lang="de" ng-app>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        
-        <title>BMI Rechner</title>
-        
-         <script src='https://code.jquery.com/jquery-3.1.0.js' integrity='sha256-slogkvB1K3VOkzAI8QITxV3VzpOnkeNVsKvtkYLMjfk=' crossorigin='anonymous'></script>
-        
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-        
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> 
-        <script src="app.js"></script>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
-    </head>
-    <body>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <header style="background-color:lightgrey;padding:10px">
-                        <h1>BMI Rechner</h1>
-                        <span style="text-size:8px">v<?=$VERSION?></span>
-                    </header>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col">
-                    <ul class="nav nav-tabs">
-                        <li class="nav-item">
-                            <a data-toggle="tab" class="nav-link <?=(isset($_SESSION['selection']) && $_SESSION['selection'] == 1 ? 'active': '' )?>" href="#metric" onclick="changeSelection(1)">Metrisch</a>
-                        </li>
-                        <li class="nav-item">
-                            <a data-toggle="tab" class="nav-link <?=(isset($_SESSION['selection']) && $_SESSION['selection'] == 2 ? 'active': '' )?>" href="#empirical" onclick="changeSelection(2)">Empirisch</a>
-                        </li>
-                    </ul>
-                    
-                    <div class="tab-content">
-                        <div id="metric" class="tab-pane container <?=(isset($_SESSION['selection']) && $_SESSION['selection'] == 1 ? 'active': '' )?>">
-                            <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-                                <div class="form-group">
-                                    <label for="weight">Gewicht (kg):</label>
-                                    <input type="number" name="weight" min="0.0" step="0.1" class="form-control" value="<?php if(isset($_SESSION['weight'])) echo $_SESSION['weight']; ?>">   
-                                </div>
-                                <div class="form-group">
-                                    <label for="height">Gr&ouml;&szlig;e (cm):</label>
-                                    <input type="number" name="height" min="0" step="1" class="form-control" value="<?php if(isset($_SESSION['height'])) echo $_SESSION['height']; ?>">   
-                                </div>
-                                <button type="submit" class="btn btn-success">Berechnen</button>
-                            </form>
-                        </div>
-                        
-                        <div id="empirical" class="tab-pane container <?=(isset($_SESSION['selection']) && $_SESSION['selection'] == 2 ? 'active': '' )?>">
-                            <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
-                                <div class="form-group">
-                                    <label for="weightEmp">Gewicht (lbs):</label>
-                                    <input type="number" name="weightEmp" min="0.0" step="0.1" class="form-control" value="<?php if(isset($_SESSION['weightEmp'])) echo $_SESSION['weightEmp']; ?>">   
-                                </div>
-                                
-                                <div class="form-group">
-                                    <label for="heightFt">Ft:</label>
-                                    <input type="number" name="heightFt" min="0" step="1" class="form-control" value="<?php if(isset($_SESSION['heightFt'])) echo $_SESSION['heightFt']; ?>">  
-                                    
-                                    <label for="heightInch">Inch:</label>
-                                    <input type="number" name="heightInch" min="0" step="1" class="form-control" value="<?php if(isset($_SESSION['heightInch'])) echo $_SESSION['heightInch']; ?>">   
-                                </div>
-                                <button type="submit" class="btn btn-success">Berechnen</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            
-            <div class="row">
-                <div class="col">
-                    <h2>
-                    <?php
-                        if(isset($_SESSION['bmi']))
-                        {
-                            echo "Ihr BMI: ".$_SESSION['bmi']." ".$_SESSION['dimension'];
-							
-                            unset($_SESSION['weight']);
-                            unset($_SESSION['height']);
-                            unset($_SESSION['weightEmp']);
-                            unset($_SESSION['heightFt']);
-                            unset($_SESSION['heightInch']);
-                            unset($_SESSION['dimension']);
-                        } 
-
-                        elseif(isset($_SESSION['error']))
-                        {
-                            echo $_SESSION['error'];
-                            unset($_SESSION['error']);
-                            
-                            if(isset($_SESSION['height']))
-                            {
-                                unset($_SESSION['weight']);
-                                unset($_SESSION['height']);   
-                            }
-                            
-                            elseif(isset($_SESSION['heightFt']))
-                            {
-                                unset($_SESSION['weightEmp']);
-                                unset($_SESSION['heightFt']);
-                                unset($_SESSION['heightInch']);
-                            }
-                        }
-                    ?>
-                    </h2>
-              	</div>
-              	<div class="col">
-                    
-                    <?php 
-                    	if(isset($_SESSION['bmi']))
-						{
-							$bmi10 = $_SESSION['bmi']*10;
-
-							echo "
-							<table class='table'>
-								<thead>
-									<th>BMI</th>
-									<th>Kategorie</th>
-								</thead>
-								<tbody>
-									<tr style='".($bmi10 < 185 ? "background-color:green" : "")."'><td>unter 18,5</td><td>Untergewicht</td></tr>
-									<tr style='".($bmi10 >= 185 && $bmi10 <= 249 ? "background-color:green" : "")."'><td>18,5 - 24,9</td><td>Normalgewicht</td></tr>
-									<tr style='".($bmi10 >= 250 && $bmi10 <= 299 ? "background-color:green" : "")."'><td>25,0 - 29,9</td><td>&Uuml;bergewicht</td></tr>
-									<tr style='".($bmi10 >= 300 && $bmi10 <= 349 ? "background-color:green" : "")."'><td>30,0 - 34,9</td><td>Fettleibigkeit 1</td></tr>
-									<tr style='".($bmi10 >= 350 && $bmi10 <= 399 ? "background-color:green" : "")."'><td>35,0 - 39,9</td><td>Fettleibigkeit 2</td></tr>
-									<tr style='".($bmi10 >= 400 ? "background-color:green" : "")."'><td>&uuml;ber 40</td><td>Fettleibigkeit 3</td></tr>
-								</tbody>
-							</table>";
-							
-							echo "<a href='http://www.euro.who.int/en/health-topics/disease-prevention/nutrition/a-healthy-lifestyle/body-mass-index-bmi' style='font-size:10px'>Quelle</a>";
-							
-							unset($_SESSION['bmi']);
-						}
-                    ?>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
